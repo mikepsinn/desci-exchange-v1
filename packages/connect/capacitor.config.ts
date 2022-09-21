7732ce7ed1fa7f606cc6fd232f06a8bc6cd73290
Mike P. Sinn <m@quantimodo.com>
2022-09-21 11:55:37 -0500

    DeSci Connect App
    Took 26 minutes

---
+++ packages/connect/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'connect',
+  webDir: '../../dist/packages/connect',
+  bundledWebRuntime: false,
+};
+
+export default config;
