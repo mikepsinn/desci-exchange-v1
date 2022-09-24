80756b9606d50dc8e46e7e63563c8fa7b987a500
Mike P. Sinn <m@quantimodo.com>
2022-09-24 00:12:48 -0500

    web3, react, and ionic libraries
    Took 5 hours 2 minutes

--- packages/connect/src/App.tsx
+++ packages/connect/src/App.tsx
@@ -7,7 +7,7 @@ import {
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
 import Menu from './components/Menu';
-import Page from './pages/Page';
+import Inbox from './pages/Inbox';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -41,7 +41,7 @@ const App: React.FC = () => {
               <Redirect to="/page/Inbox" />
             </Route>
             <Route path="/page/:name" exact={true}>
-              <Page />
+              <Inbox />
             </Route>
           </IonRouterOutlet>
         </IonSplitPane>
