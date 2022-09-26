67a72d583d8dd3e2d96d6d230d913b81cb648413
Mike P. Sinn <m@quantimodo.com>
2022-09-26 17:05:47 -0500

    Storage utility
    Took 2 minutes

--- packages/desci-exchange/storage/index.ts
+++ packages/desci-exchange/storage/index.ts
@@ -1,34 +1,8 @@
-
-import Storage from './Storage'
-
-
-export const storage = new Storage(window.localStorage, '')
-
-export const getStoragePrefix = (): string => {
-
-  return `_desci_`
+import Storage from './Storage';
+let storage: any;
+if (typeof window !== 'undefined') {
+  storage = new Storage(window.localStorage, '');
+} else {
+  storage = {};
 }
-
-export const loadFromStorage = <T = unknown>(key: string, prefix = getStoragePrefix()): T | undefined => {
-  return storage.getItem(`${prefix}${key}`)
-}
-
-export const saveToStorage = <T = unknown>(key: string, value: T): void => {
-  storage.setItem<T>(`${getStoragePrefix()}${key}`, value)
-}
-
-export const removeFromStorage = (key: string): void => {
-  storage.removeItem(`${getStoragePrefix()}${key}`)
-}
-
-export const saveToStorageWithExpiry = <T = unknown>(key: string, value: T, expiry: number): void => {
-  storage.setWithExpiry<T>(key, value, expiry)
-}
-
-export const loadFromStorageWithExpiry = <T = unknown>(key: string): T | undefined => {
-  return storage.getWithExpiry<T>(key)
-}
-
-export const removeFromStorageWithExpiry = (key: string): void => {
-  return storage.removeItem(key)
-}
+export default storage;
